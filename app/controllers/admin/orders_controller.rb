d5b3058614d3464ee825e824e4613afb6717c00a
SegueX <free1989xj@hotmail.com>
2017-06-26 22:43:12 +0800

    orders graph

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,6 +7,20 @@ class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.all.order("id DESC").paginate(:page => params[:page], :per_page => 25 )   #equals to @orders = Order.order("id DESC")
 
+    if @orders.any?
+      dates = (Order.all.order("id ASC").first.created_at.to_date..Date.today).to_a
+
+      @data = {
+        labels: dates,
+        datasets: [{
+          label: "# of Orders",
+          data: dates.map{ |d| @orders.where("created_at >= ? AND created_at <= ?", d.beginning_of_day, d.end_of_day).count},
+          borderColor: "#ff6384"
+          }]
+      }
+
+    end
+
   end
 
   def show
